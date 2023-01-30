0797aa60ea52c955dfd402b3b31bc3af52235170
Hoshimado <xingyanhuan@yahoo.co.jp>
2023-01-30 12:44:08 +0900

    Feature/#7 make it easy to see popup (#8)
    * #7 ダイアログを連続して複数回表示後にタイムアウトする仕様に変更
    * #7 C#フォームを利用して明示的に最前面に表示するように変更した

---
+++ investigation/msgbox-timeout.ps1
@@ -0,0 +1,79 @@
+﻿# Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned
+
+
+# https://forums.powershell.org/t/show-opup-with-timeout-and-focus/15982/3
+#
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+# 利用する「.NET Framwork（のアセンブリ）」を明示的にロードする。
+# ※デフォルトロードされるものもあり、その場合はそのまま利用可能。
+#   ただし、期待のアセンブリが「デフォルト」に入っているとは
+#   限らないので、利用するものを明示的にロードするのが良い。
+# 
+# ref.
+# 実行環境でロードされているアセンブリを確認するには「[System.AppDomain]::CurrentDomain.GetAssemblies()」を使います。
+#  [System.AppDomain]::CurrentDomain.GetAssemblies() | % { $_.GetName().Name }
+# https://www.vwnet.jp/windows/PowerShell/CheckAssemblis.htm
+function messageDialogWithTimeout ($timeout,$message)
+{
+
+    # フォームControlを作成し、サイズを指定する
+    $form = New-Object System.Windows.Forms.Form
+    $form.Text = "Company Name"
+    $form.Size = New-Object System.Drawing.Size(300,200)
+    $form.StartPosition = 'CenterScreen'
+
+    # ラベルControlを作成して、任意のテキストを設定し、
+    # それをフォームControlの上に貼り付け。
+    $label = New-Object System.Windows.Forms.label
+    $label.Text = $message
+    # Locationは省略で「(0,0)」の位置から張り付ける
+    $label.Size = New-Object System.Drawing.Size(200,40)
+    $form.Controls.Add($label)
+
+    # ボタンControlを作成し、（以下略）
+    # ボタン押下時の戻り値に「DialogResult::OK」を指定。
+    $okButton = New-Object System.Windows.Forms.Button
+    $okButton.Location = New-Object System.Drawing.Point(75,120)
+    $okButton.Size = New-Object System.Drawing.Size(75,23)
+    $okButton.Text = 'OK'
+    $okButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
+    $form.AcceptButton = $okButton
+    $form.Controls.Add($okButton)
+
+    # タイマーを設定。
+    # トリガーされたら、自身を停止後にフォームControlを閉じる。
+    $timer = New-Object System.Windows.Forms.Timer
+    $timer.Interval = $timeout * 1000
+    $timer.add_tick({ # ToDo: この記法は「J#」か？　「C#」では記法が異なるのでは？
+        $timer.Stop();
+        $form.Close();
+        return;
+    })
+    $timer.Start()
+
+    # フォームControlの表示位置（z軸）を「最前面」に指定
+    $form.Topmost = $true
+
+    # フォーム終了時にタイマーを無効にするように設定
+    # ToDo: Stop()の方が適切なのでは？
+    $form.add_formclosed({ # ToDo: この記法は「J#」か？　「C#」では記法が異なるのでは？
+        $timer.Enabled = $false;
+    })
+
+    # フォームを表示
+    $result = $form.ShowDialog()
+
+    # フォームを破棄
+    $form.Dispose()
+
+    return $result
+}
+
+$result = messageDialogWithTimeout -message “This is the message you will see on the window`nMessage on new line” -timeout 5
+
+if([System.Windows.Forms.DialogResult]::OK -ne $result){
+    Write-Output "続行OK以外"
+}else{
+    Write-Output "OKボタンが押された"
+}
--- message-sleep-battery.ps1
+++ message-sleep-battery.ps1
@@ -25,13 +25,15 @@
 
 
 # 「作業を続ける？（寝落ちしてない？）」の確認ダイアログを出し始めるバッテリー残量の閾値
-Set-Variable -Name THRESHOLD_BATTERY_CHARGE_REMAINING -Value 30 -Option Constant
+Set-Variable -Name THRESHOLD_BATTERY_CHARGE_REMAINING -Value 50 -Option Constant
 
 # （未実装）繰り返し確認ダイアログを出す場合の、バッテリー残量の間隔
 # Set-Variable -Name INTERVAL_OF_BATTERY_CHARGE_VALUE -Value 10 -Option Constant
 
 # 確認ダイアログで待機する秒数（この秒数を超えて応答が無ければ「寝落ちしている」と判断）
-Set-Variable -Name WAIT_FOR_DIALOG_RESPONSE_SEC -Value 180 -Option Constant
+# と、通知への気づきの観点で、繰返し表示する回数。実際は「秒数×回数」が待機秒数、となる。
+Set-Variable -Name WAIT_FOR_DIALOG_RESPONSE_SEC -Value 60 -Option Constant
+Set-Variable -Name NUMBER_OF_REPEAT_DIALOG      -Value  3 -Option Constant
 
 # スクリプトのタイトル（ダイアログのダイアログ）
 Set-Variable -Name TITLE_TEXT -Value "寝落ち時スリープ移行支援Ver.0.01" -Option Constant
@@ -78,20 +80,97 @@ Get-CimInstance -ClassName Win32_Battery | Select-Object -Property DeviceID, Est
 
 
 
+# https://forums.powershell.org/t/show-opup-with-timeout-and-focus/15982/3
+#
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+# 利用する「.NET Framwork（のアセンブリ）」を明示的にロードする。
+# ※デフォルトロードされるものもあり、その場合はそのまま利用可能。
+#   ただし、期待のアセンブリが「デフォルト」に入っているとは
+#   限らないので、利用するものを明示的にロードするのが良い。
+# 
+# ref.
+# 実行環境でロードされているアセンブリを確認するには「[System.AppDomain]::CurrentDomain.GetAssemblies()」を使います。
+#  [System.AppDomain]::CurrentDomain.GetAssemblies() | % { $_.GetName().Name }
+# https://www.vwnet.jp/windows/PowerShell/CheckAssemblis.htm
+function messageDialogWithTimeout ($timeout,$message,$titleText)
+{
 
+    # フォームControlを作成し、サイズを指定する
+    $form = New-Object System.Windows.Forms.Form
+    $form.Text = $titleText
+    $form.Size = New-Object System.Drawing.Size(360,200)
+    $form.StartPosition = 'CenterScreen'
+
+    # ラベルControlを作成して、任意のテキストを設定し、
+    # それをフォームControlの上に貼り付け。
+    $label = New-Object System.Windows.Forms.label
+    $label.Text = $message
+    $label.Location = New-Object System.Drawing.Point(8,8)
+    $label.Size = New-Object System.Drawing.Size(304,108)
+    $form.Controls.Add($label)
+
+    # ボタンControlを作成し、（以下略）
+    # ボタン押下時の戻り値に「DialogResult::OK」を指定。
+    $okButton = New-Object System.Windows.Forms.Button
+    $okButton.Location = New-Object System.Drawing.Point(132,120)
+    $okButton.Size = New-Object System.Drawing.Size(75,23)
+    $okButton.Text = 'OK'
+    $okButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
+    $form.AcceptButton = $okButton
+    $form.Controls.Add($okButton)
+
+    # タイマーを設定。
+    # トリガーされたら、自身を停止後にフォームControlを閉じる。
+    $timer = New-Object System.Windows.Forms.Timer
+    $timer.Interval = $timeout * 1000
+    $timer.add_tick({ # ToDo: この記法は「J#」か？　「C#」では記法が異なるのでは？
+        $timer.Stop();
+        $form.Close();
+        return;
+    })
+    $timer.Start()
+
+    # フォームControlの表示位置（z軸）を「最前面」に指定
+    $form.Topmost = $true
+
+    # フォーム終了時にタイマーを無効にするように設定
+    # ToDo: Stop()の方が適切なのでは？
+    $form.add_formclosed({ # ToDo: この記法は「J#」か？　「C#」では記法が異なるのでは？
+        $timer.Enabled = $false;
+    })
+
+    # フォームを表示
+    $result = $form.ShowDialog()
+
+    # フォームを破棄
+    $form.Dispose()
+
+    return $result
+}
+
+# https://www.tekizai.net/entry/powershell_messagebox_1
+#                    vbYesNo = 4
+# Set-Variable -Name vbYes         -Value  6 -Option Constant
+# Set-Variable -Name POPUP_TIMEOUT -Value -1 -Option Constant
+# $wsobj = new-object -comobject wscript.shell
+# $wsobj.popup() は使わなくなったが、暫しコメントに残しておく。
 function Confirm-Popup4ContinueWorking {
   param (
     $titleText, $nSecondsToWait, $EstimatedRunTime, $EstimatedChargeRemaining
   )
 
-  $wsobj = new-object -comobject wscript.shell
   $msgText = "バッテリー残り時間は" + $EstimatedRunTime + "分です`n作業を継続しますか？`n`n※一定時間内に「はい」が押されない場合は、スリープ状態へ移行します。"
 
-  Return $wsobj.popup($msgText, $nSecondsToWait, $titleText, "4")
-  # https://www.tekizai.net/entry/powershell_messagebox_1
-  # vbYesNo = 4
+  $i = $NUMBER_OF_REPEAT_DIALOG -1
+  do {
+    $result = messageDialogWithTimeout -message $msgText -titleText $titleText -timeout $nSecondsToWait
+
+    $i--
+  } while (($i -ge 0) -and ([System.Windows.Forms.DialogResult]::OK -ne $result))
+  
+  Return $result
 }
-Set-Variable -Name vbYes -Value 6 -Option Constant
 
 function output-ActionsLog {
   param (
@@ -117,7 +196,7 @@ function output-ActionsLog {
 If($EstimatedChargeRemaining -lt $THRESHOLD_BATTERY_CHARGE_REMAINING){
   $result = Confirm-Popup4ContinueWorking ($TITLE_TEXT) ($WAIT_FOR_DIALOG_RESPONSE_SEC) ($EstimatedRunTime) ($EstimatedChargeRemaining)
 
-  If($result -ne $vbYes){
+  If($result -ne [System.Windows.Forms.DialogResult]::OK){
     output-ActionsLog ($EstimatedChargeRemaining) ($ACT_TIMEOUT) 
 
     Add-Type -Assembly System.Windows.Forms;[System.Windows.Forms.Application]::SetSuspendState(‘Suspend’, $false, $false);
